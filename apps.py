73087f64d396b61a30630d9d590f38b9d92cc517
Aomsin29 <101800977+Aomsin29@users.noreply.github.com>
2022-09-30 21:57:10 +0700

    Add files via upload

---
+++ apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AomappConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'Aomapp'
---
+++ views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse('<h1>Hello World</h1>')
+
+
+def homepage(request):
+    return render(request, 'Aomapp/home.html')
+# Create your views here.
